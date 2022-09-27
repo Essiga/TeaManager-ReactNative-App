d3384ff276135394db1bbbb7e9cd3fa16d1cd72f
prescherm <m.prescher@gantner-instruments.com>
2022-09-27 15:00:52 +0300

    !

---
+++ components/view/TeaView.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+const TeaView = () => {
+  return <View><Text>TeaView</Text></View>;
+};
+
+export default TeaView;
---
+++ components/view/VesselView.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+const VesselView = () => {
+  return <View><Text>VesselView</Text></View>;
+};
+
+export default VesselView;
