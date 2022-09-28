c0a841d4e05bd93188ddc2b1787ff3db9e37e11b
prescherm <m.prescher@gantner-instruments.com>
2022-09-28 15:03:12 +0300

    new expo project

--- App.tsx
+++ App.tsx
@@ -1,51 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
- import * as React from 'react';
- import {
-     // SafeAreaView,
-     // StyleSheet,
-     // Text,
-     // useColorScheme,
-     // View,
- } from 'react-native';
- import { NavigationContainer } from '@react-navigation/native';
- import { createDrawerNavigator } from '@react-navigation/drawer';
- import TeaView from './components/view/TeaView';
- import VesselView from './components/view/VesselView';
- // import {Colors} from 'react-native/Libraries/NewAppScreen';
- // import Main from './components/Main';
- 
- const Drawer = createDrawerNavigator();
- 
- const App = () => {
-     // const isDarkMode = useColorScheme() === 'dark';
- 
-     // const backgroundStyle = {
-     //     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-     // };
- 
-     // return (
-     //     <SafeAreaView style={backgroundStyle}>
-     //       <Main/>
-     //     </SafeAreaView>
-     // );
- 
-     return (
-         <NavigationContainer>
-             <Drawer.Navigator initialRouteName="TeaView">
-                 <Drawer.Screen name="TeaView" component={TeaView} />
-                 <Drawer.Screen name="VesselView" component={VesselView} />
-             </Drawer.Navigator>
-         </NavigationContainer>
-     );
- };
- 
- export default App;
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+      </SafeAreaProvider>
+    );
+  }
+}
--- components/view/VesselView.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-
-const VesselView = () => {
-  return <View><Text>VesselView</Text></View>;
-};
-
-export default VesselView;
