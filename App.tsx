89f7236d90f265cb59c632f40e82c6839807aa32
prescherm <m.prescher@gantner-instruments.com>
2022-10-11 17:12:23 +0300

    removed expo-status-bar

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
-import {StatusBar} from 'expo-status-bar';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Provider as PaperProvider} from 'react-native-paper';
-
-import Theme from './constants/Theme'
 import useCachedResources from './hooks/useCachedResources';
 import Navigation from './navigation';
+import Theme from './constants/Theme'
 import 'react-native-url-polyfill/auto';
 
 export default function App() {
@@ -17,7 +15,6 @@ export default function App() {
             <PaperProvider theme={Theme}>
                 <SafeAreaProvider>
                     <Navigation/>
-                    <StatusBar/>
                 </SafeAreaProvider>
             </PaperProvider>
         );
