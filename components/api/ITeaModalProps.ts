e6bce5571b3b6c44f3f1da83f8ec6b33d6e66252
inderro1@students.zhaw.ch <inderro1@students.zhaw.ch>
2022-10-12 13:59:57 +0300

    adjust the new layout to new code

--- components/api/ITeaModalProps.ts
+++ components/api/ITeaModalProps.ts
@@ -4,4 +4,5 @@ export interface ITeaModalProps {
     tea: Tea;
     toggleTeaModalVisibility: (visibility: boolean) => void;
     toggleAddSessionModalVisibility:(visibility: boolean) => void;
+    setUpdateTeaVisible(visibility: boolean): void;
 }
